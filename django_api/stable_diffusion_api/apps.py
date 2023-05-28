f2d830e8a899f09c044bdbff8360c4b1626635b9
Chen Xiang <scxxiang@gmail.com>
2023-05-28 16:43:25 +0800

    Added initial file

---
+++ django_api/stable_diffusion_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StableDiffusionApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stable_diffusion_api'
---
+++ image_genie_app/android/app/src/main/kotlin/com/example/image_genie_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_genie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
