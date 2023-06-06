2ee682f1e83e651490fc4dfa4f674cc2493886cd
Chen Xiang <scxxiang@gmail.com>
2023-06-06 18:40:32 +0800

    Update .gitignore

---
+++ image_genie_app/lib/main.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(ImageGenieApp());
+}
+
+class ImageGenieApp extends StatelessWidget {
+  const ImageGenieApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          child: Center(child: Text("Hello")),
+        ),
+      ),
+    );
+  }
+}
