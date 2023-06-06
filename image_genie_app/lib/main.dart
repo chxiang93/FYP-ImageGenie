dfe61a90d8fe41d2539b10e5aef0c2f3cb3b9c39
Chen Xiang <scxxiang@gmail.com>
2023-06-06 23:40:03 +0800

    Completed splash page

--- image_genie_app/lib/main.dart
+++ image_genie_app/lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'splash_page.dart';
 
 void main() {
     runApp(ImageGenieApp());
@@ -9,9 +10,9 @@ class ImageGenieApp extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Container()
+        home: SplashPage()
     );
   }
 }
---
+++ image_genie_app/lib/splash_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashPage extends StatelessWidget {
+    const SplashPage({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: const Color(0xFF0025AA),
+            body: Stack(
+              children: [
+                  Center(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                            Image.asset("assets/logo_blue.png", width: 200, height: 200,),
+                            const SizedBox(height: 5),
+                            Text(
+                                "ImageGenie",
+                                style: GoogleFonts.pacifico(
+                                    fontSize: 40,
+                                    color: Colors.white,
+                                ),
+                            ),
+                            const SizedBox(height: 50,),
+                            const SizedBox(
+                                width: 70,
+                                height: 70,
+                                child: CircularProgressIndicator(
+                                    strokeWidth: 8,
+                                    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                              ),
+                            )
+                        ],
+                      ),
+                  ),
+                ],
+            ),
+        );
+    }
+}
