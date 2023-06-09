1710bfb6ffa4ad14182c5fb30daf812149d2b326
Chen Xiang <scxxiang@gmail.com>
2023-06-09 18:26:29 +0800

    Refactor code to include MultiProvider in the root

--- image_genie_app/lib/main.dart
+++ image_genie_app/lib/main.dart
@@ -1,12 +1,21 @@
 import "package:flutter/material.dart";
+import 'package:provider/provider.dart';
 import 'cartoonify_page.dart';
 import 'neural_transfer_page.dart';
 import 'splash_page.dart';
 import 'home_page.dart';
 import 'super_resolution_page.dart';
+import 'services.dart';
 
 void main() {
-    runApp(const ImageGenieApp());
+    runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ImageUploadService(),
+        ),
+      ],
+      child: const ImageGenieApp(),
+    ));
 }
 
 class ImageGenieApp extends StatelessWidget {
