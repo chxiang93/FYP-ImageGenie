8921eb4ba4966ccf8300ca944a3bbe0e7d2069ef
Chen Xiang <scxxiang@gmail.com>
2023-08-25 12:13:11 +0800

    Improve loading widget ui

--- image_genie_app/lib/utils.dart
+++ image_genie_app/lib/utils.dart
@@ -367,22 +367,36 @@ class LoadingWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        width: 80,
-        height: 80,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Color(0xFFBDBDBD).withOpacity(0.1),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Colors.black, width: 3.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              offset: const Offset(1.0, 2.0),
+              blurRadius: 5.0,
+              spreadRadius: 3.0
+            ),
+          ],
+        ),
+        width: 150,
+        height: 150,
         child: Stack(
           children: [
             Center(
               child: SizedBox(
-                width: 80,
-                height: 80,
+                width: 90,
+                height: 90,
                 child: CircularProgressIndicator(
-                  strokeWidth: 8.0,
+                  strokeWidth: 10.0,
                   valueColor: AlwaysStoppedAnimation(Utils.mainColor),
                 ),
               ),
             ),
             Center(
-              child: Image.asset("assets/logo_black.png", width: 40, height: 40,),
+              child: Image.asset("assets/logo_black.png", width: 50, height: 50,),
             ),
           ],
         ),
