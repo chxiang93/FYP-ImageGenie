2401157fcbb269cf0e2d3f0d21288d579992c0ca
Chen Xiang <scxxiang@gmail.com>
2023-09-14 19:31:37 +0800

    backend: clean up code

--- django_api/cartoonify_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import CartoonifyView
-
-urlpatterns = [
-    path("upload/", CartoonifyView.as_view(), name="cartoonify-upload"),
-]
--- django_api/stable_diffusion_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StableDiffusionApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stable_diffusion_api'
